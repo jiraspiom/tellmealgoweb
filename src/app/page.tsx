8f67468980f203569ed73f7248418eb44484a864
1gilmar <1gilmar@gmail.com>
2023-06-28 22:48:01 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
 
   return (
     <div>
-      <div className="h-screen">
+      <div className="">
         <Texto value={texto} onChange={handleOnChange} />
       </div>
       <div>
--- src/components/Texto.tsx
+++ src/components/Texto.tsx
@@ -6,7 +6,7 @@ interface iTexto {
 export function Texto({ value, onChange }: iTexto) {
   return (
     <div>
-      <div className="mb-2 mt-2  px-2 pt-2">
+      <div className="mb-2 mt-2 px-2 pt-2">
         <textarea
           value={value}
           onChange={(e) => onChange(e.target.value)}
