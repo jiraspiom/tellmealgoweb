ba1c7e277f4a248cae6ae470140da1cfd44e2664
1gilmar <1gilmar@gmail.com>
2023-07-08 16:18:23 -0300

    ajustado

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -13,7 +13,7 @@ export function Botao({ onClick, novaCor }: iBotao) {
     <div>
       <button
         onClick={onClick}
-        className={` mb-2 mr-2 mt-2 rounded-full bg-blue-400 px-8 py-2 font-bold text-white hover:bg-blue-500 ${novaCor}`}
+        className={`mb-2 mr-2  mt-2 rounded-full  bg-blue-400 px-8 py-2 font-bold text-white hover:bg-blue-500 ${novaCor}`}
       >
         Hi :)
       </button>
