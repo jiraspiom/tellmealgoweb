fbb9c38483d6daa5e5f9e9773d6e58c202d67712
1gilmar <1gilmar@gmail.com>
2025-01-14 08:50:56 -0300

    criado novo projeto

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+
 ## Getting Started
 
 First, run the development server:
@@ -8,27 +10,27 @@ npm run dev
 yarn dev
 # or
 pnpm dev
+# or
+bun dev
 ```
 
-## commands
-> npx create-next-app@latest web --use-npm
-# yes for all
-> npm i @rocketseat/eslint-config -D
-> npm i prettier-plugin-tailwindcss -D
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-# Ajustar arquivo
-adicionar o "@rocketseat/eslint-config/react" com array ao arquivo eslintrc.json
-criar arquivo prettier.config.json e add
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
-```
-module.exports = {
-  plugin: [require('prettier-plugin-tailwindcss')],
-}
-```
+This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
 
-## Build and run:
-```
-docker build -t web-to-docker . 
-docker run -d -p 8088:3000 --name web-to-docker web-to-docker
-```
-Open `http://localhost:8088` in your browser.
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,146 +1,101 @@
-'use client'
+import Image from "next/image";
 
-import { Botao } from '@/components/Botao'
-import { Card } from '@/components/Card'
-import { Carregando } from '@/components/Carregando'
-import { Indicador } from '@/components/Indicador'
-import { Texto } from '@/components/Texto'
-import { useState, useEffect } from 'react'
-import { fetchData, postData } from './utils/api'
-
-interface Dado {
-  cor: string
-  coracao: null
-  dataAt: string
-  id: string
-  segredo: string
-  urlImage: string
-  __v: number
-}
-
-export default function Opa() {
-  const [texto, setTexto] = useState('')
-  const [data, setData] = useState<Dado[]>([])
-  const [cor, setCor] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [page, setPage] = useState(1)
-  const [hasMore, setHasMore] = useState(true)
-
-  const handleOnChange = (text: string) => {
-    setTexto(text)
-  }
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const responseData = await fetchData()
-        const newData = responseData.slice(0, 8)
-        setData(newData)
-        setPage(1)
-        setHasMore(responseData.length > 8)
-        console.log(responseData)
-        console.log(newData)
-        console.log(responseData.length > 8)
-      } catch (error) {
-        console.error('Erro ao carregar os dados:', error)
-      }
-    }
-    getData()
-  }, [])
-
-  const handlePostData = async () => {
-    try {
-      const response = await postData({ segredo: texto, cor })
-
-      if (response.status === 200) {
-        const novoDado = response.data
-        setData((prevData) => [novoDado, ...prevData])
-        setTexto('')
-      } else {
-        console.error('Erro ao adicionar o dado:', response.status)
-      }
-    } catch (error) {
-      console.error('Erro ao enviar a requisição POST:', error)
-    }
-  }
-
-  const handleIndicadorClick = (corNova: string) => {
-    setCor(corNova)
-  }
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (
-        window.innerHeight + document.documentElement.scrollTop >=
-        document.documentElement.offsetHeight - 100
-      ) {
-        loadMore()
-      }
-    }
-
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  })
-
-  const loadMore = async () => {
-    if (!loading && hasMore) {
-      setLoading(true)
-      try {
-        const responseData = await fetchData()
-        const startIndex = page * 8
-        const endIndex = startIndex + 8
-        const newData = responseData.slice(startIndex, endIndex)
-        setData((prevData) => [...prevData, ...newData])
-        setPage((prevPage) => prevPage + 1)
-        setHasMore(responseData.length > endIndex)
-      } catch (error) {
-        console.error('Erro ao carregar mais dados:', error)
-      }
-      setLoading(false)
-    }
-  }
-
+export default function Home() {
   return (
-    <div>
-      <div className="">
-        <Texto value={texto} onChange={handleOnChange} />
-      </div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
 
-      <div className="flex flex-row">
-        <div className="flex items-center">
-          <Botao onClick={handlePostData} novaCor={cor} />
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
-
-        <div className="flex items-center ">
-          <Indicador onClick={handleIndicadorClick} />
-        </div>
-      </div>
-
-      <hr className="border-4 border-gray-800" />
-      <div>
-        {data.length > 0 ? (
-          <ul>
-            {data.map((item, index) => (
-              <li key={index}>
-                <Card
-                  descricao={item.segredo}
-                  url={item.urlImage}
-                  cor={item.cor}
-                />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <div className="items-center">
-            <Carregando />
-          </div>
-        )}
-        {loading && (
-          <div className="items-center">
-            <Carregando />
-          </div>
-        )}
-      </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
-  )
+  );
 }
