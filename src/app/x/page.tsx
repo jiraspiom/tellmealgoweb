71f79b1f7336fd2c986bc6a2311b9a2ca88d6a20
1gilmar <1gilmar@gmail.com>
2025-03-28 15:26:51 -0300

    buildd

--- src/app/x/page.tsx
+++ src/app/x/page.tsx
@@ -1,20 +1,15 @@
-import TwitterX from "@/components/Twitter";
+import TwitterX from '@/components/Twitter'
 
 export default function X() {
+  console.log('object')
   return (
-    <main
-      className="min-h-screen flex justify-center items-center"
-    >
-      <div
-        className="w-full max-w-xl mx-auto border-t border-l border-r border-gray-200"
-      >
+    <main className="min-h-screen flex justify-center items-center">
+      <div className="w-full max-w-xl mx-auto border-t border-l border-r border-gray-200">
         <TwitterX />
       </div>
     </main>
-
   )
 }
 
 //justify-center ajusta no centro da tela x
 // items-center ajusta no centro da tela y
-
